787f0dd222afcebad0309207fbfbc275f36f2fe1
ihsansfd <ihsannurul@hotmail.com>
2022-09-20 08:09:10 +0700

    first

---
+++ assets/src/js/custom/script.js
@@ -0,0 +1,9 @@
+const siteNavbar = document.querySelector(".site-navbar");
+
+const onScrollSetNavbarToLight = (event) => {
+  console.log(siteNavbar.offsetHeight);
+};
+
+document.addEventListener("scroll", onScrollSetNavbarToLight);
+
+console.log("HALLLLLLLLLLLLLLLLLLLLLLLLl");
